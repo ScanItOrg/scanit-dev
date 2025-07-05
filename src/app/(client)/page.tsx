c89634c449b5f82766a21025b256a210d25339b5
Sumit Kumar <sumitkumarracer@gmail.com>
2025-07-05 23:17:09 +0530

    [ADD]

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -6,44 +6,12 @@ export const metadata = {
     description: "A good company will always be connected to use."
 }
 
-type Company = {
-  id: number;
-  name: string;
-  description: string;
-  logo_url: string;
-};
 
-async function getCompanies(): Promise<Company[]> {
-  const API_URL = process.env.API_URL;
-
-  if (!API_URL) throw new Error("Missing API_URL in environment");
-
-  const res = await fetch(`${API_URL}/api/companies`, {
-    next: { revalidate: 60 }
-  });
-
-  if (!res.ok) throw new Error("Failed to fetch companies");
-
-  return res.json();
-}
-
-export default async function CompaniesPage() {
-  const companies = await getCompanies();
+export default function CompaniesPage() {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {companies.map((company) => (
-        <Link key={company.id} href={`/${company.id}`}>
-          <Card key={company.id} className="hover:shadow-xl transition-shadow">
-            <CardHeader>
-              <CardTitle>{company.name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p>{company.description}</p>
-            </CardContent>
-          </Card>
-        </Link>
-      ))}
+      
     </div>
   );
 }
