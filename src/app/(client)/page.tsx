fd4f8dbfa504888f68b708dc1be0980d6b522151
sagar_dev <Sagar Kumar@MILAD.LOCAL>
2025-07-07 23:40:02 +0530

    [UPDATE] update files

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,5 +1,3 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import Link from "next/link";
 
 export const metadata = {
     title: "Companies",
--- src/app/(server)/api/user-manual/[id]/route.ts
+++ src/app/(server)/api/user-manual/[id]/route.ts
@@ -28,6 +28,6 @@ export async function GET(
 
     return NextResponse.json(company);
   } catch (error) {
-    return NextResponse.json({ error: "Server error" }, { status: 500 });
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
